b5b371e93c7fa6cab36f00d758c4b120fe0f69db
Izzat Alsharif <work@izzatalsharif.com>
2024-07-18 16:40:11 +0300

    Move controllers to presentation layer

--- TravelAccommodationBookingPlatform.App/Program.cs
+++ TravelAccommodationBookingPlatform.App/Program.cs
@@ -1,8 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddApplicationPart(TravelAccommodationBookingPlatform.Presentation.AssemblyReference.Assembly);
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- TravelAccommodationBookingPlatform.Presentation/Controllers/WeatherForecastController.cs
+++ TravelAccommodationBookingPlatform.Presentation/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-namespace TravelAccommodationBookingPlatform.App.Controllers;
+namespace TravelAccommodationBookingPlatform.Presentation.Controllers;
 
 [ApiController]
 [Route("[controller]")]
