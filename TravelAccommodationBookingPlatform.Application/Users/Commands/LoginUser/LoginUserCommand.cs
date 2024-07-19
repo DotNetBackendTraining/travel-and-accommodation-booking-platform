e87048c135fe599c5b7be953a0c4f2c64457751e
Izzat Alsharif <work@izzatalsharif.com>
2024-07-20 01:28:12 +0300

    Convert command to class

--- TravelAccommodationBookingPlatform.Application/Users/Commands/LoginUser/LoginUserCommand.cs
+++ TravelAccommodationBookingPlatform.Application/Users/Commands/LoginUser/LoginUserCommand.cs
@@ -2,7 +2,8 @@ using TravelAccommodationBookingPlatform.Application.Interfaces.Messaging;
 
 namespace TravelAccommodationBookingPlatform.Application.Users.Commands.LoginUser;
 
-public record LoginUserCommand(
-    string Username,
-    string Password)
-    : ICommand<LoginUserResponse>;
+public class LoginUserCommand : ICommand<LoginUserResponse>
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
