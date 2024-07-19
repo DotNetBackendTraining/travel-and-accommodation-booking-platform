1f654953a91cd4122df6ee47e140cbf3655e4b99
Izzat Alsharif <work@izzatalsharif.com>
2024-07-19 16:27:27 +0300

    TABP-26 Add generic entity and set dates on context save changes

---
+++ TravelAccommodationBookingPlatform.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace TravelAccommodationBookingPlatform.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
--- TravelAccommodationBookingPlatform.Persistence/AppDbContext.cs
+++ TravelAccommodationBookingPlatform.Persistence/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TravelAccommodationBookingPlatform.Domain.Entities;
 
 namespace TravelAccommodationBookingPlatform.Persistence;
 
@@ -6,5 +7,33 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+    }
+
+    public override int SaveChanges()
+    {
+        SetTimestamps();
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SetTimestamps();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void SetTimestamps()
+    {
+        var entries = ChangeTracker.Entries().Where(e =>
+            e is { Entity: BaseEntity, State: EntityState.Added or EntityState.Modified });
+        foreach (var entry in entries)
+        {
+            var now = DateTime.UtcNow;
+            ((BaseEntity)entry.Entity).UpdatedAt = now;
+
+            if (entry.State == EntityState.Added)
+            {
+                ((BaseEntity)entry.Entity).CreatedAt = now;
+            }
+        }
     }
 }
