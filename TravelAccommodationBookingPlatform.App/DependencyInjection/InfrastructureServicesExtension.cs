a8aa3fc95753f74f408b985af52cf07ec8022bec
Izzat Alsharif <work@izzatalsharif.com>
2024-07-19 22:23:35 +0300

    TABP-55 Register JWT Authentication services

--- TravelAccommodationBookingPlatform.App/DependencyInjection/InfrastructureServicesExtension.cs
+++ TravelAccommodationBookingPlatform.App/DependencyInjection/InfrastructureServicesExtension.cs
@@ -1,3 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using TravelAccommodationBookingPlatform.Application.Interfaces;
+using TravelAccommodationBookingPlatform.Infrastructure.Services;
 using TravelAccommodationBookingPlatform.Infrastructure.Settings;
 
 namespace TravelAccommodationBookingPlatform.App.DependencyInjection;
@@ -7,5 +13,28 @@ public static class InfrastructureServicesExtension
     public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                var serviceProvider = services.BuildServiceProvider();
+                var jwtSettings = serviceProvider.GetRequiredService<IOptions<JwtSettings>>().Value;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+                };
+            });
     }
 }
