785c69ceecf915aca5223f415b2c5a02d2507563
Izzat Alsharif <work@izzatalsharif.com>
2024-07-20 20:04:15 +0300

    TABP-49 Add registration endpoint

--- TravelAccommodationBookingPlatform.Presentation/Controllers/UserController.cs
+++ TravelAccommodationBookingPlatform.Presentation/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TravelAccommodationBookingPlatform.Application.Users.Commands.LoginUser;
+using TravelAccommodationBookingPlatform.Application.Users.Commands.RegisterUser;
 using TravelAccommodationBookingPlatform.Domain.Shared;
 using TravelAccommodationBookingPlatform.Presentation.Controllers.ResultExtensions;
 
@@ -41,5 +42,30 @@ public class UserController : ControllerBase
         return result.IsFailure
             ? result.ToUnauthorizedProblemDetails()
             : Ok(result.Value);
+    }
+
+    /// <summary>
+    /// Registers a new user in the system.
+    /// </summary>
+    /// <param name="command">Register user command with user details.</param>
+    /// <returns>Response indicating the result of the registration process.</returns>
+    /// <response code="201">User created successfully.</response>
+    /// <response code="422">If the request is invalid (validation error).</response>
+    /// <response code="401">Unauthorized if operation is not allowed.</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Register([FromBody] RegisterUserCommand command)
+    {
+        var result = await _sender.Send(command);
+        if (result is IValidationResult validationResult)
+        {
+            return validationResult.ToValidationProblemDetails();
+        }
+
+        return result.IsFailure
+            ? result.ToUnauthorizedProblemDetails()
+            : Created();
     }
 }
