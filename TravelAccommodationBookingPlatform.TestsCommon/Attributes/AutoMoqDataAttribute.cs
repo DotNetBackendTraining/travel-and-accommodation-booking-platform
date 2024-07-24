a983a7897cd5b172cbb22dbae36a9017566473a5
Izzat Alsharif <work@izzatalsharif.com>
2024-07-24 14:31:16 +0300

    TABP-104 Add omit on recursion option to AutoMoqDataAttribute

--- TravelAccommodationBookingPlatform.TestsCommon/Attributes/AutoMoqDataAttribute.cs
+++ TravelAccommodationBookingPlatform.TestsCommon/Attributes/AutoMoqDataAttribute.cs
@@ -6,7 +6,22 @@ namespace TravelAccommodationBookingPlatform.TestsCommon.Attributes;
 
 public class AutoMoqDataAttribute : AutoDataAttribute
 {
-    public AutoMoqDataAttribute() : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+    public AutoMoqDataAttribute(bool omitOnRecursion = false)
+        : base(() => CustomizeFixture(new Fixture(), omitOnRecursion))
     {
+    }
+
+    private static Fixture CustomizeFixture(Fixture fixture, bool omitOnRecursion)
+    {
+        fixture.Customize(new AutoMoqCustomization());
+
+        if (omitOnRecursion)
+        {
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        return fixture;
     }
 }
