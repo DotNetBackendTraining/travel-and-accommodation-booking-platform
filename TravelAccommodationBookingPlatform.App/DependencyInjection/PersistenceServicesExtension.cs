c3e9c724ff9de6e1b16c3862a516751b8ecda27e
Izzat Alsharif <work@izzatalsharif.com>
2024-07-24 18:27:58 +0300

    TABP-109 Set queries as non tracking by default

--- TravelAccommodationBookingPlatform.App/DependencyInjection/PersistenceServicesExtension.cs
+++ TravelAccommodationBookingPlatform.App/DependencyInjection/PersistenceServicesExtension.cs
@@ -10,8 +10,11 @@ public static class PersistenceServicesExtension
     public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             options.UseSqlServer(configuration.GetConnectionString("AppDbContextConnection") ??
-                                 throw new ArgumentException("AppDbContextConnection not found")));
+                                 throw new ArgumentException("AppDbContextConnection not found"));
+        });
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository, UserRepository>();
