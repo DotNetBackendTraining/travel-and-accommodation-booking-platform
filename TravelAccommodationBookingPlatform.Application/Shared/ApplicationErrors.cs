fdf7121ec545e2a2245a4900efa5a47712d4f06c
Izzat Alsharif <work@izzatalsharif.com>
2024-07-26 21:43:21 +0300

    TABP-143 Add file errors

---
+++ TravelAccommodationBookingPlatform.Application/Shared/ApplicationErrors.cs
@@ -0,0 +1,19 @@
+using TravelAccommodationBookingPlatform.Domain.Shared;
+
+namespace TravelAccommodationBookingPlatform.Application.Shared;
+
+public static class ApplicationErrors
+{
+    public static class File
+    {
+        public static readonly Error UploadFailed = new(
+            ErrorType.InternalServerError,
+            "File.UploadFailed",
+            "An error occurred while uploading the file");
+
+        public static readonly Error DeletionFailed = new(
+            ErrorType.InternalServerError,
+            "File.DeletionFailed",
+            "An error occurred while deleting the file");
+    }
+}
