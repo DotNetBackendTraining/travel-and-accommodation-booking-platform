3c4781b0e2c0b2581e5219141f7bdeeffa6d4c41
Izzat Alsharif <work@izzatalsharif.com>
2024-07-28 00:29:39 +0300

    Add ImageExtensions constant

---
+++ TravelAccommodationBookingPlatform.Application/Constants/ApplicationRules.cs
@@ -0,0 +1,9 @@
+namespace TravelAccommodationBookingPlatform.Application.Constants;
+
+public static class ApplicationRules
+{
+    public static class File
+    {
+        public const string ImageExtensions = "jpg,jpeg,png";
+    }
+}
