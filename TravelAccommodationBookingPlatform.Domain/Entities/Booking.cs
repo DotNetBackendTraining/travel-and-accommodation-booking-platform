0678d7a76f956a65f0273a2eaa887743961bf415
Izzat Alsharif <work@izzatalsharif.com>
2024-07-30 01:48:05 +0300

    Fix unnecessary nullable

--- TravelAccommodationBookingPlatform.Domain/Entities/Booking.cs
+++ TravelAccommodationBookingPlatform.Domain/Entities/Booking.cs
@@ -9,7 +9,7 @@ public class Booking : BaseEntity
     public Checking Checking { get; set; } = default!;
     public ICollection<Room> Rooms { get; set; } = default!;
     public NumberOfGuests NumberOfGuests { get; set; } = default!;
-    public SpecialRequest? SpecialRequest { get; set; }
+    public SpecialRequest SpecialRequest { get; set; } = default!;
     public Guid? PaymentId { get; set; }
     public Payment? Payment { get; set; }
 }
