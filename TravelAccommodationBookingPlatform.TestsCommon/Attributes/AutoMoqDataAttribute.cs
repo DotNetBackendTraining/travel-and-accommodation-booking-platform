a778213d71527f3673f44acfc535c6ba6da05499
Izzat Alsharif <work@izzatalsharif.com>
2024-08-01 15:41:33 +0300

    TABP-214 Add Customizations Params to AutoMoqDataAttribute

--- TravelAccommodationBookingPlatform.TestsCommon/Attributes/AutoMoqDataAttribute.cs
+++ TravelAccommodationBookingPlatform.TestsCommon/Attributes/AutoMoqDataAttribute.cs
@@ -7,11 +7,16 @@ namespace TravelAccommodationBookingPlatform.TestsCommon.Attributes;
 public class AutoMoqDataAttribute : AutoDataAttribute
 {
     public AutoMoqDataAttribute(bool omitOnRecursion = false)
-        : base(() => CustomizeFixture(new Fixture(), omitOnRecursion))
+        : base(() => CustomizeFixture(new Fixture(), omitOnRecursion, []))
     {
     }
 
-    private static Fixture CustomizeFixture(Fixture fixture, bool omitOnRecursion)
+    public AutoMoqDataAttribute(bool omitOnRecursion = false, params Type[] customizations)
+        : base(() => CustomizeFixture(new Fixture(), omitOnRecursion, customizations))
+    {
+    }
+
+    private static Fixture CustomizeFixture(Fixture fixture, bool omitOnRecursion, IEnumerable<Type> customizations)
     {
         fixture.Customize(new AutoMoqCustomization());
 
@@ -20,6 +25,16 @@ public class AutoMoqDataAttribute : AutoDataAttribute
             fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
                 .ForEach(b => fixture.Behaviors.Remove(b));
             fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+        }
+
+        foreach (var customization in customizations)
+        {
+            if (typeof(ICustomization).IsAssignableFrom(customization))
+            {
+                var instance = Activator.CreateInstance(customization) ??
+                               throw new InvalidOperationException("Could not create customization instance");
+                fixture.Customize((ICustomization)instance);
+            }
         }
 
         return fixture;
