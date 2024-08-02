27331b192fee9028a2cf67f0d56a2fa964afffb5
Izzat Alsharif <work@izzatalsharif.com>
2024-08-02 18:02:32 +0300

    Fix actions by delaying start

--- TravelAccommodationBookingPlatform.IntegrationTests/Shared/IntegrationTestWebAppFactory.cs
+++ TravelAccommodationBookingPlatform.IntegrationTests/Shared/IntegrationTestWebAppFactory.cs
@@ -1,4 +1,6 @@
+using DotNet.Testcontainers.Containers;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.SqlClient;
 using Testcontainers.MsSql;
 
 namespace TravelAccommodationBookingPlatform.IntegrationTests.Shared;
@@ -12,6 +14,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
     public async Task InitializeAsync()
     {
         await _container.StartAsync();
+        await WaitForContainerToBeReady(_container);
         Environment.SetEnvironmentVariable(
             "ConnectionStrings:AppDbContextConnection",
             _container.GetConnectionString());
@@ -21,5 +24,28 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
     {
         await _container.DisposeAsync();
         await base.DisposeAsync();
+    }
+
+    private static async Task WaitForContainerToBeReady(IDatabaseContainer container)
+    {
+        var timeout = TimeSpan.FromMinutes(5);
+        var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+        while (stopwatch.Elapsed < timeout)
+        {
+            try
+            {
+                await using var connection = new SqlConnection(container.GetConnectionString());
+                await connection.OpenAsync();
+                return;
+            }
+            catch
+            {
+                // Wait for a bit before retrying
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        throw new Exception("The container did not become ready within the specified timeout.");
     }
 }
