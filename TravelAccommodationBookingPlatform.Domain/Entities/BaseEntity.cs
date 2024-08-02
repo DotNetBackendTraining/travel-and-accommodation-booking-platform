d506b5b1d2eae2ff4160bf8f7ec193683f02c481
Izzat Alsharif <work@izzatalsharif.com>
2024-08-02 16:25:47 +0300

    TABP-217 Remove default value of EntityBase Id field

--- TravelAccommodationBookingPlatform.Domain/Entities/BaseEntity.cs
+++ TravelAccommodationBookingPlatform.Domain/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace TravelAccommodationBookingPlatform.Domain.Entities;
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
