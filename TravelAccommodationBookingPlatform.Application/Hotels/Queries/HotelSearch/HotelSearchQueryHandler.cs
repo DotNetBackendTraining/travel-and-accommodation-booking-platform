afa817c79a037ab1f98ecc0212e1c57a81295ba6
Izzat Alsharif <work@izzatalsharif.com>
2024-08-04 02:22:55 +0300

    TABP-174 Add HotelSearchQueryHandler

---
+++ TravelAccommodationBookingPlatform.Application/Hotels/Queries/HotelSearch/HotelSearchQueryHandler.cs
@@ -0,0 +1,38 @@
+using AutoMapper;
+using TravelAccommodationBookingPlatform.Application.Hotels.Queries.HotelSearch.Specifications;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Messaging;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Repositories;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+using TravelAccommodationBookingPlatform.Domain.Shared;
+
+namespace TravelAccommodationBookingPlatform.Application.Hotels.Queries.HotelSearch;
+
+public class HotelSearchQueryHandler : IQueryHandler<HotelSearchQuery, HotelSearchResponse>
+{
+    private readonly IRepository<Hotel> _repository;
+    private readonly IMapper _mapper;
+
+    public HotelSearchQueryHandler(
+        IRepository<Hotel> repository,
+        IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<HotelSearchResponse>> Handle(HotelSearchQuery request, CancellationToken cancellationToken)
+    {
+        var resultsSpec = new HotelSearchResultsSpecification(request.Filters, _mapper);
+        var resultsPage = await _repository.PageAsync(resultsSpec, request.PaginationParameters, cancellationToken);
+        var response = new HotelSearchResponse { SearchResults = resultsPage ?? new() };
+
+        if (request.IncludeSearchResultFilters)
+        {
+            var hotelsSpec = new HotelSearchSpecification(request.Filters);
+            var filtersSpec = new AvailableFiltersSpecification();
+            response.AvailableFilters = await _repository.AggregateAsync(hotelsSpec, filtersSpec, cancellationToken);
+        }
+
+        return Result.Success(response);
+    }
+}
