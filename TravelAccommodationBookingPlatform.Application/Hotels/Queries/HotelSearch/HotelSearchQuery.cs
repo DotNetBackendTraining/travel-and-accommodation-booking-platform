6887d6264d5adbb9af53ee737d436e6166869d1d
Izzat Alsharif <work@izzatalsharif.com>
2024-08-04 01:45:34 +0300

    TABP-171 Design HotelSearch Query and Response

---
+++ TravelAccommodationBookingPlatform.Application/Hotels/Queries/HotelSearch/HotelSearchQuery.cs
@@ -0,0 +1,46 @@
+using TravelAccommodationBookingPlatform.Application.Interfaces.Messaging;
+using TravelAccommodationBookingPlatform.Application.Shared.Pagination;
+using TravelAccommodationBookingPlatform.Domain.Enums;
+using TravelAccommodationBookingPlatform.Domain.ValueObjects;
+
+namespace TravelAccommodationBookingPlatform.Application.Hotels.Queries.HotelSearch;
+
+public class HotelSearchQuery : IQuery<HotelSearchResponse>
+{
+    public HotelSearchFilters Filters { get; set; } = new();
+    public PaginationParameters PaginationParameters { get; set; } = default!;
+    public bool IncludeSearchResultFilters { get; set; }
+
+    public class HotelSearchFilters
+    {
+        public string SearchTerm { get; set; } = string.Empty;
+        public GeneralFilters General { get; set; } = new();
+        public AdvancedFilters Advanced { get; set; } = new();
+    }
+
+    public class GeneralFilters
+    {
+        public Checking Checking { get; set; } = new()
+        {
+            CheckInDate = DateTime.Today,
+            CheckOutDate = DateTime.Today.AddDays(1)
+        };
+
+        public NumberOfGuests NumberOfGuests { get; set; } = new()
+        {
+            Adults = 2,
+            Children = 0
+        };
+
+        public int Rooms { get; set; } = 1;
+    }
+
+    public class AdvancedFilters
+    {
+        public Price? MinPrice { get; set; }
+        public Price? MaxPrice { get; set; }
+        public List<StarRate>? AllowedStarRatings { get; set; }
+        public List<Amenity>? RequiredAmenities { get; set; }
+        public List<RoomType>? RequiredRoomTypes { get; set; }
+    }
+}
---
+++ TravelAccommodationBookingPlatform.Application/Hotels/Queries/HotelSearch/HotelSearchResponse.cs
@@ -0,0 +1,38 @@
+using TravelAccommodationBookingPlatform.Application.Shared.Pagination;
+using TravelAccommodationBookingPlatform.Domain.Enums;
+using TravelAccommodationBookingPlatform.Domain.ValueObjects;
+
+namespace TravelAccommodationBookingPlatform.Application.Hotels.Queries.HotelSearch;
+
+public class HotelSearchResponse
+{
+    public PageResponse<HotelSearchResult> SearchResults { get; set; } = default!;
+    public AvailableFiltersResult? AvailableFilters { get; set; }
+
+    public class HotelSearchResult
+    {
+        public HotelSummary HotelSummary { get; set; } = default!;
+        public Price? MinimumPrice { get; set; }
+        public Price? MaximumPrice { get; set; }
+    }
+
+    public class HotelSummary
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public StarRate StarRate { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public Image ThumbnailImage { get; set; } = default!;
+        public Guid CityId { get; set; }
+        public string CityName { get; set; } = string.Empty;
+    }
+
+    public class AvailableFiltersResult
+    {
+        public Price MinimumPrice { get; set; } = default!;
+        public Price MaximumPrice { get; set; } = default!;
+        public IList<StarRate> AvailableStarRatings { get; set; } = default!;
+        public IList<Amenity> AvailableAmenities { get; set; } = default!;
+        public IList<RoomType> AvailableRoomTypes { get; set; } = default!;
+    }
+}
