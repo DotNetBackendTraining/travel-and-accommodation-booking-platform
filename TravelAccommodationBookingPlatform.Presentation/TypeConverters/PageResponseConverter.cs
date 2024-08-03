88bdcf47a991dca0335bb321032ef681a09b5e4f
Izzat Alsharif <work@izzatalsharif.com>
2024-08-04 02:31:36 +0300

    TABP-172 Add PageResponseConverter

---
+++ TravelAccommodationBookingPlatform.Presentation/TypeConverters/PageResponseConverter.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using TravelAccommodationBookingPlatform.Application.Shared.Pagination;
+
+namespace TravelAccommodationBookingPlatform.Presentation.TypeConverters;
+
+public class PageResponseConverter<TSource, TDestination>
+    : ITypeConverter<PageResponse<TSource>, PageResponse<TDestination>>
+{
+    public PageResponse<TDestination> Convert(
+        PageResponse<TSource> source,
+        PageResponse<TDestination> destination,
+        ResolutionContext context)
+    {
+        return new PageResponse<TDestination>
+        {
+            Items = source.Items.Select(item => context.Mapper.Map<TDestination>(item)).ToList(),
+            TotalCount = source.TotalCount
+        };
+    }
+}
