8777a6e3a65bcf2af4c56d565904bf282eb28406
Izzat Alsharif <work@izzatalsharif.com>
2024-08-05 01:26:20 +0300

    TABP-229 Add test for CitySearchQueryHandler

---
+++ TravelAccommodationBookingPlatform.Application.UnitTests/Cities/Queries/Handlers/CitySearchQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+using AutoFixture.Xunit2;
+using Moq;
+using TravelAccommodationBookingPlatform.Application.Cities.Queries.CitySearch;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Repositories;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+using TravelAccommodationBookingPlatform.TestsCommon.Attributes;
+
+namespace TravelAccommodationBookingPlatform.Application.UnitTests.Cities.Queries.Handlers;
+
+public class CitySearchQueryHandlerTests
+{
+    [Theory, AutoMoqData]
+    public async Task Handle_CallsPageAsync_WithCorrectParameters(
+        CitySearchQuery query,
+        [Frozen] Mock<IRepository<City>> mockRepository,
+        CitySearchQueryHandler handler)
+    {
+        // Arrange
+        query.PaginationParameters.PageNumber = 1;
+        query.PaginationParameters.PageSize = 10;
+
+        // Act
+        await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        mockRepository.Verify(r => r.PageAsync(
+                It.IsAny<CitySearchSpecification>(),
+                query.PaginationParameters,
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
