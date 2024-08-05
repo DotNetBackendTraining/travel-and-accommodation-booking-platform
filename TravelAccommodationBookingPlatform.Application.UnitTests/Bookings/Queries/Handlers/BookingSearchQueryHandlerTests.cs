42992b490571bbdc21681fc1941f4bcb78a10107
Izzat Alsharif <work@izzatalsharif.com>
2024-08-05 23:14:06 +0300

    TABP-186 Add BookingSearchQueryHandlerTests

---
+++ TravelAccommodationBookingPlatform.Application.UnitTests/Bookings/Queries/Handlers/BookingSearchQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+using AutoFixture.Xunit2;
+using Moq;
+using TravelAccommodationBookingPlatform.Application.Bookings.Queries.BookingSearch;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Repositories;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+using TravelAccommodationBookingPlatform.TestsCommon.Attributes;
+
+namespace TravelAccommodationBookingPlatform.Application.UnitTests.Bookings.Queries.Handlers;
+
+public class BookingSearchQueryHandlerTests
+{
+    [Theory, AutoMoqData]
+    public async Task Handle_CallsPageAsync_WithCorrectParameters(
+        BookingSearchQuery query,
+        [Frozen] Mock<IRepository<Booking>> mockRepository,
+        BookingSearchQueryHandler handler)
+    {
+        // Arrange
+        query.PaginationParameters.PageNumber = 1;
+        query.PaginationParameters.PageSize = 10;
+
+        // Act
+        await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        mockRepository.Verify(r => r.PageAsync(
+                It.IsAny<BookingSearchSpecification>(),
+                query.PaginationParameters,
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
