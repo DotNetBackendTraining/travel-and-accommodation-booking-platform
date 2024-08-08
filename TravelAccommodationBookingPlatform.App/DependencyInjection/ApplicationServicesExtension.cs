0f45705fdf224bc09801bd57cc953b5748125d67
Izzat Alsharif <work@izzatalsharif.com>
2024-08-09 01:56:33 +0300

    TABP-245 Add DI extension method for ITransactionHandler

--- TravelAccommodationBookingPlatform.App/DependencyInjection/ApplicationServicesExtension.cs
+++ TravelAccommodationBookingPlatform.App/DependencyInjection/ApplicationServicesExtension.cs
@@ -1,6 +1,8 @@
 using FluentValidation;
 using MediatR;
 using TravelAccommodationBookingPlatform.Application.Behaviors;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Messaging;
+using TravelAccommodationBookingPlatform.Application.Shared.Notifications;
 
 namespace TravelAccommodationBookingPlatform.App.DependencyInjection;
 
@@ -16,5 +18,19 @@ public static class ApplicationServicesExtension
         services.AddValidatorsFromAssembly(Application.AssemblyReference.Assembly);
 
         services.AddAutoMapper(Application.AssemblyReference.Assembly);
+    }
+
+    /// <summary>
+    /// Use this to register an ITransactionHandler,
+    /// otherwise MediatR will a separate transient service to handle each notification
+    /// </summary>
+    internal static void AddScopedTransactionHandler(
+        this IServiceCollection services,
+        Func<IServiceProvider, ITransactionHandler> implementationFactory)
+    {
+        services.AddScoped<INotificationHandler<TransactionStartNotification>>(implementationFactory);
+        services.AddScoped<INotificationHandler<TransactionSuccessNotification>>(implementationFactory);
+        services.AddScoped<INotificationHandler<TransactionFailureNotification>>(implementationFactory);
+        services.AddScoped<INotificationHandler<TransactionCleanupNotification>>(implementationFactory);
     }
 }
