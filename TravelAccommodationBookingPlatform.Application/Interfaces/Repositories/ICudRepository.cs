44254cf1c3dfbf8d154f52e2abd802b907d61acf
Izzat Alsharif <work@izzatalsharif.com>
2024-08-09 01:56:55 +0300

    Add documentation

--- TravelAccommodationBookingPlatform.Application/Interfaces/Repositories/ICudRepository.cs
+++ TravelAccommodationBookingPlatform.Application/Interfaces/Repositories/ICudRepository.cs
@@ -2,6 +2,9 @@ using TravelAccommodationBookingPlatform.Domain.Entities;
 
 namespace TravelAccommodationBookingPlatform.Application.Interfaces.Repositories;
 
+/// <summary>
+/// Provides basic Create, Update, and Delete (CUD) operations for entities within a unit of work transaction.
+/// </summary>
 public interface ICudRepository<in TEntity> where TEntity : BaseEntity
 {
     void Add(TEntity entity);
