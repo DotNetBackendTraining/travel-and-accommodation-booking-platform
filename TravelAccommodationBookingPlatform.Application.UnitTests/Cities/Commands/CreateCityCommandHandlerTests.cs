4187cad4b7b88c021674fd605abda15f97a15abf
Izzat Alsharif <work@izzatalsharif.com>
2024-08-09 22:33:37 +0300

    TABP-247 Add tests for CreateCityCommandHandler

---
+++ TravelAccommodationBookingPlatform.Application.UnitTests/Cities/Commands/CreateCityCommandHandlerTests.cs
@@ -0,0 +1,51 @@
+using System.Linq.Expressions;
+using AutoFixture.Xunit2;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using TravelAccommodationBookingPlatform.Application.Cities.Commands.CreateCity;
+using TravelAccommodationBookingPlatform.Application.Interfaces;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Repositories;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+using TravelAccommodationBookingPlatform.Domain.ValueObjects;
+using TravelAccommodationBookingPlatform.TestsCommon.Attributes;
+
+namespace TravelAccommodationBookingPlatform.Application.UnitTests.Cities.Commands;
+
+public class CreateCityCommandHandlerTests
+{
+    [Theory, AutoMoqData(omitOnRecursion: true)]
+    public async Task Handle_SuccessfulCityCreation_ReturnsSuccess(
+        [Frozen] Mock<ICudRepository<City>> cudRepositoryMock,
+        [Frozen] Mock<IImageRepository> imageRepositoryMock,
+        [Frozen] Mock<IUnitOfWork> unitOfWorkMock,
+        [Frozen] Mock<IMapper> mapperMock,
+        CreateCityCommand request,
+        City city,
+        CreateCityCommandHandler handler)
+    {
+        // Arrange
+        mapperMock.Setup(m => m.Map<City>(request)).Returns(city);
+
+        cudRepositoryMock.Setup(repo => repo.Add(It.IsAny<City>()));
+        imageRepositoryMock.Setup(repo => repo
+            .SaveAndUpdate(request.ThumbnailImage, city, It.IsAny<Expression<Func<City, Image>>>()));
+        unitOfWorkMock.Setup(uow => uow.SaveChangesAsync(It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Id.Should().Be(city.Id);
+
+        cudRepositoryMock.Verify(repo => repo.Add(city),
+            Times.Once);
+        imageRepositoryMock.Verify(repo => repo
+                .SaveAndUpdate(request.ThumbnailImage, city, It.IsAny<Expression<Func<City, Image>>>()),
+            Times.Once);
+        unitOfWorkMock.Verify(uow => uow.SaveChangesAsync(It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
