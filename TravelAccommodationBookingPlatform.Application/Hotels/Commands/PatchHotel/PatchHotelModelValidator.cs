58470672850c9ecfaff7ac0207480d5a43c4cd57
Izzat Alsharif <work@izzatalsharif.com>
2024-08-09 15:14:11 +0300

    TABP-148 Add PatchHotelModelValidator

---
+++ TravelAccommodationBookingPlatform.Application/Hotels/Commands/PatchHotel/PatchHotelModelValidator.cs
@@ -0,0 +1,36 @@
+using FluentValidation;
+using TravelAccommodationBookingPlatform.Application.Validators;
+using TravelAccommodationBookingPlatform.Application.Validators.Extensions;
+
+namespace TravelAccommodationBookingPlatform.Application.Hotels.Commands.PatchHotel;
+
+public class PatchHotelModelValidator : AbstractValidator<PatchHotelModel>
+{
+    public PatchHotelModelValidator()
+    {
+        RuleFor(x => x.Name!)
+            .NotNull().WithMessage("Name field cannot be removed")
+            .ValidHotelName();
+
+        RuleFor(x => x.StarRate)
+            .NotNull().WithMessage("StarRate field cannot be removed");
+
+        RuleFor(x => x.Description!)
+            .NotNull().WithMessage("Description field cannot be removed")
+            .ValidHotelDescription();
+
+        RuleFor(x => x.Owner!)
+            .NotNull().WithMessage("Owner field cannot be removed")
+            .ValidOwner();
+
+        RuleFor(x => x.Coordinates!)
+            .NotNull().WithMessage("Coordinates field cannot be removed")
+            .SetValidator(new CoordinatesValidator());
+
+        RuleFor(x => x.Amenities)
+            .NotNull().WithMessage("Amenities field cannot be removed");
+
+        RuleFor(x => x.CityId)
+            .NotNull().WithMessage("CityId field cannot be removed");
+    }
+}
