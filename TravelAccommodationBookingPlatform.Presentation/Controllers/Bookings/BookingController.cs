ab7fb48e60158d6229b07ef8d039e62dbaf3b215
Izzat Alsharif <work@izzatalsharif.com>
2024-08-12 17:36:51 +0300

    TABP-257 Add BookingDetails endpoint

--- TravelAccommodationBookingPlatform.Presentation/Controllers/Bookings/BookingController.cs
+++ TravelAccommodationBookingPlatform.Presentation/Controllers/Bookings/BookingController.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TravelAccommodationBookingPlatform.Application.Bookings.Queries.BookingDetails;
+using TravelAccommodationBookingPlatform.Application.Bookings.Queries.BookingDetails.DTOs;
 using TravelAccommodationBookingPlatform.Application.Bookings.Queries.BookingSearch;
 using TravelAccommodationBookingPlatform.Presentation.Attributes;
 using TravelAccommodationBookingPlatform.Presentation.Controllers.Bookings.Requests;
@@ -48,6 +50,41 @@ public class BookingController : AbstractController
 
         var query = Mapper.Map<BookingSearchQuery>(request);
         query.UserId = userIdResult.Value;
+
+        return await HandleQueryResult(query, cancellationToken);
+    }
+
+    /// <summary>
+    /// Retrieves the details of a specific booking for the user.
+    /// </summary>
+    /// <param name="id">The unique identifier of the booking.</param>
+    /// <param name="parameters">The request containing options of the booking details to return.</param>
+    /// <param name="cancellationToken">Cancellation token for the request.</param>
+    /// <returns>The details of the requested booking.</returns>
+    /// <response code="200">Returns the details of the booking.</response>
+    /// <response code="401">Unauthorized if credentials are invalid.</response>
+    /// <response code="404">If the booking is not found for the user.</response>
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(BookingDetailsResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<BookingDetailsResponse>> GetBookingDetails(
+        [FromRoute] Guid id,
+        [FromQuery] BookingDetailsParameters parameters,
+        CancellationToken cancellationToken)
+    {
+        var userIdResult = GetUserIdOrFailure();
+        if (userIdResult.IsFailure)
+        {
+            return userIdResult.ToProblemDetails();
+        }
+
+        var query = new BookingDetailsQuery
+        {
+            Id = id,
+            UserId = userIdResult.Value,
+            Parameters = parameters
+        };
 
         return await HandleQueryResult(query, cancellationToken);
     }
