96d8d3f6974b4aac535a375891cb491209521db6
Izzat Alsharif <work@izzatalsharif.com>
2024-08-12 17:31:13 +0300

    TABP-257 Move final price calculation logic to Price ValueObject

--- TravelAccommodationBookingPlatform.Application/Shared/Responses/PriceDealResponse.cs
+++ TravelAccommodationBookingPlatform.Application/Shared/Responses/PriceDealResponse.cs
@@ -6,11 +6,5 @@ public class PriceDealResponse
 {
     public Price OriginalPrice { get; set; } = default!;
     public DiscountRate DiscountRate { get; set; } = new() { Percentage = 0 };
-    public Price FinalPrice => CalculateFinalPrice();
-
-    private Price CalculateFinalPrice()
-    {
-        var discountAmount = OriginalPrice.Value * (DiscountRate.Percentage / 100);
-        return new Price { Value = OriginalPrice.Value - discountAmount };
-    }
+    public Price FinalPrice => OriginalPrice.CalculateFinalPrice(DiscountRate);
 }
--- TravelAccommodationBookingPlatform.Domain/ValueObjects/Price.cs
+++ TravelAccommodationBookingPlatform.Domain/ValueObjects/Price.cs
@@ -1,6 +1,19 @@
+using TravelAccommodationBookingPlatform.Domain.Entities;
+
 namespace TravelAccommodationBookingPlatform.Domain.ValueObjects;
 
 public class Price
 {
     public double Value { get; set; }
+
+    public Price CalculateFinalPrice(Discount? discount)
+    {
+        return discount is null ? this : CalculateFinalPrice(discount.Rate);
+    }
+
+    public Price CalculateFinalPrice(DiscountRate rate)
+    {
+        var discountAmount = Value * (rate.Percentage / 100);
+        return new Price { Value = Value - discountAmount };
+    }
 }
