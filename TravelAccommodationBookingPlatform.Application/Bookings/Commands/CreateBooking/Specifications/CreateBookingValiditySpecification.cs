13d2a575df475201755793beb04a2435b2178b27
Izzat Alsharif <work@izzatalsharif.com>
2024-08-12 22:16:21 +0300

    TABP-259 Add CreateBooking conditional specifications

---
+++ TravelAccommodationBookingPlatform.Application/Bookings/Commands/CreateBooking/Specifications/CreateBookingValiditySpecification.cs
@@ -0,0 +1,19 @@
+using Ardalis.Specification;
+using TravelAccommodationBookingPlatform.Application.Interfaces.Specifications;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+using TravelAccommodationBookingPlatform.Domain.ValueObjects;
+
+namespace TravelAccommodationBookingPlatform.Application.Bookings.Commands.CreateBooking.Specifications;
+
+public sealed class CreateBookingValiditySpecification : AggregateSpecification<Room, bool>
+{
+    public CreateBookingValiditySpecification(NumberOfGuests numberOfGuests)
+    {
+        Query.Select(g =>
+            // All rooms belong to the same hotel
+            g.All(r => r.HotelId == g.First().HotelId) &&
+            // Rooms can accommodate the total number of guests
+            g.Sum(r => r.MaxNumberOfGuests.Adults) >= numberOfGuests.Adults &&
+            g.Sum(r => r.MaxNumberOfGuests.Children) >= numberOfGuests.Children);
+    }
+}
