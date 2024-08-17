f5915c32072aa265e1a37d38b8dee4b2a6be745a
Izzat Alsharif <work@izzatalsharif.com>
2024-08-17 20:00:42 +0300

    TABP-265 Add Autofac Service Provider

--- TravelAccommodationBookingPlatform.App/Program.cs
+++ TravelAccommodationBookingPlatform.App/Program.cs
@@ -1,16 +1,19 @@
+using Autofac.Extensions.DependencyInjection;
 using Serilog;
 using TravelAccommodationBookingPlatform.App.DependencyInjection;
 using TravelAccommodationBookingPlatform.App.WebApplicationExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddPresentationServices();
-
-builder.Host.UseSerilog((context, configuration) =>
-    configuration.ReadFrom.Configuration(context.Configuration));
 
 if (builder.Environment.IsDevelopment())
 {
