104ab28c4206830eb0d500676847378ba868a49c
Izzat Alsharif <work@izzatalsharif.com>
2024-08-20 23:27:14 +0300

    TABP-161 Add AdminHotelDetailsSpecification

---
+++ TravelAccommodationBookingPlatform.Application/Hotels/Queries/HotelDetails/Admin/AdminHotelDetailsSpecification.cs
@@ -0,0 +1,23 @@
+using Ardalis.Specification;
+using AutoMapper;
+using TravelAccommodationBookingPlatform.Domain.Entities;
+
+namespace TravelAccommodationBookingPlatform.Application.Hotels.Queries.HotelDetails.Admin;
+
+public sealed class AdminHotelDetailsSpecification : Specification<Hotel, AdminHotelDetailsResponse>
+{
+    public AdminHotelDetailsSpecification(AdminHotelDetailsQuery query, IMapper mapper)
+    {
+        Query.Select(h => new AdminHotelDetailsResponse
+            {
+                HotelDetails = mapper.Map<HotelDetailsResponse>(h),
+                CreatedAt = h.CreatedAt,
+                UpdatedAt = h.UpdatedAt,
+                Owner = h.Owner,
+                NumberOfRooms = h.Rooms.Count
+            })
+            .Include(h => h.City)
+            .Include(h => h.ThumbnailImage)
+            .Where(h => h.Id == query.Id);
+    }
+}
