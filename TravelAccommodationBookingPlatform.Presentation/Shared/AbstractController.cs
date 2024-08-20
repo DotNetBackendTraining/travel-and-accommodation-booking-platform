2193851f4342ddf0ab93b0c30fe262f7e02986c2
Izzat Alsharif <work@izzatalsharif.com>
2024-08-20 23:28:43 +0300

    TABP-161 Add UserRole helper methods to AbstractController

--- TravelAccommodationBookingPlatform.Presentation/Shared/AbstractController.cs
+++ TravelAccommodationBookingPlatform.Presentation/Shared/AbstractController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
 using TravelAccommodationBookingPlatform.Application.Interfaces.Messaging;
 using TravelAccommodationBookingPlatform.Domain.Constants;
+using TravelAccommodationBookingPlatform.Domain.Enums;
 using TravelAccommodationBookingPlatform.Domain.Shared;
 using TravelAccommodationBookingPlatform.Presentation.Constants;
 using TravelAccommodationBookingPlatform.Presentation.Shared.ResultExtensions;
@@ -49,5 +50,31 @@ public abstract class AbstractController : ControllerBase
         return userIdClaim is null
             ? Result.Failure<Guid>(DomainErrors.User.CredentialsNotProvided)
             : Result.Success(Guid.Parse(userIdClaim.Value));
+    }
+
+    protected Result<UserRole> GetUserRoleOrFailure()
+    {
+        var userRoleClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+        return userRoleClaim is null
+            ? Result.Failure<UserRole>(DomainErrors.User.CredentialsNotProvided)
+            : Result.Success(Enum.Parse<UserRole>(userRoleClaim.Value));
+    }
+
+    protected async Task<ObjectResult> HandleUserRoleResult(
+        Func<Task<ObjectResult>> adminResultFactory,
+        Func<Task<ObjectResult>> userResultFactory)
+    {
+        var userRoleResult = GetUserRoleOrFailure();
+        if (userRoleResult.IsFailure)
+        {
+            return userRoleResult.ToProblemDetails();
+        }
+
+        if (userRoleResult.Value is UserRole.Admin)
+        {
+            return await adminResultFactory();
+        }
+
+        return await userResultFactory();
     }
 }
