b5c8292f2cc93535786337b2b39db04d0b5a0677
Izzat Alsharif <work@izzatalsharif.com>
2024-08-21 00:15:02 +0300

    TABP-163 Add CityDetailsViewModel with profile

--- TravelAccommodationBookingPlatform.Presentation/Controllers/Cities/CityProfile.cs
+++ TravelAccommodationBookingPlatform.Presentation/Controllers/Cities/CityProfile.cs
@@ -1,6 +1,9 @@
 using AutoMapper;
 using TravelAccommodationBookingPlatform.Application.Cities.Commands.CreateCity;
+using TravelAccommodationBookingPlatform.Application.Cities.Queries.CityDetails;
+using TravelAccommodationBookingPlatform.Application.Cities.Queries.CityDetails.Admin;
 using TravelAccommodationBookingPlatform.Presentation.Controllers.Cities.Requests;
+using TravelAccommodationBookingPlatform.Presentation.Controllers.Cities.ViewModels;
 
 namespace TravelAccommodationBookingPlatform.Presentation.Controllers.Cities;
 
@@ -8,6 +11,13 @@ public class CityProfile : Profile
 {
     public CityProfile()
     {
+        // CreateCity
         CreateMap<CreateCityRequest, CreateCityCommand>();
+
+        // CityDetails
+        CreateMap<CityDetailsResponse, CityDetailsViewModel>();
+        CreateMap<CityDetailsResponse, AdminCityDetailsViewModel>();
+        CreateMap<AdminCityDetailsResponse, AdminCityDetailsViewModel>()
+            .IncludeMembers(src => src.CityDetails);
     }
 }
