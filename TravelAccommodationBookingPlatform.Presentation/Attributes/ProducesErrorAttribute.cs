649fbe657816f8a2bfa25a24e9d7f14a8348bb82
Izzat Alsharif <work@izzatalsharif.com>
2024-08-21 17:34:36 +0300

    Move ErrorResponse to ProducesErrorAttribute where its used exclusively

--- TravelAccommodationBookingPlatform.Presentation/Attributes/ProducesErrorAttribute.cs
+++ TravelAccommodationBookingPlatform.Presentation/Attributes/ProducesErrorAttribute.cs
@@ -1,15 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
-using TravelAccommodationBookingPlatform.Presentation.Shared;
 
 namespace TravelAccommodationBookingPlatform.Presentation.Attributes;
 
+/// <summary>
+/// Use this attribute when you want to document an Error Status Code.
+/// It assumes that the object-result schema is <see cref="ProblemDetailsWithErrorsResponse"/>-like object.
+/// </summary>
 public class ProducesErrorAttribute : ProducesResponseTypeAttribute
 {
-    public ProducesErrorAttribute(int statusCode) : base(typeof(ErrorResponse), statusCode)
+    public ProducesErrorAttribute(int errorStatusCode)
+        : base(typeof(ProblemDetailsWithErrorsResponse), errorStatusCode)
     {
-        if (statusCode < 400)
+        if (errorStatusCode < 400)
         {
-            throw new InvalidOperationException($"{statusCode} is not an error status code");
+            throw new InvalidOperationException($"{errorStatusCode} is not an error status code");
         }
+    }
+}
+
+/// <summary>
+/// Custom ProblemDetails object with Errors extension.
+/// Defines error response type that controller endpoints typically produce.
+/// </summary>
+public sealed class ProblemDetailsWithErrorsResponse
+{
+    public string Type { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+    public List<Error> Errors { get; set; } = [];
+
+    public sealed class Error
+    {
+        public string Code { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
     }
 }
